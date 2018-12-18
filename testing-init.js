b351e56c7d2d07cd3026a660f64827f41c4e4f1c
aa.malcev2 <aa.malcev2@tensor.ru>
2018-12-18 11:59:36 +0300

    Configure testing use package.json only. https://online.sbis.ru/opendoc.html?guid=60af32ed-4e9a-4cf5-af03-e6cf3212ace3

--- testing-init.js
+++ testing-init.js
@@ -1,5 +1,5 @@
 /* global testing */
 testing.configure = function() {
-   mocha.globals([ 'previousPageURL']);//надо будет разобраться где используется previousPageURL
+   mocha.globals(['previousPageURL']);//надо будет разобраться где используется previousPageURL
    mocha.checkLeaks();
 };
