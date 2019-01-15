60fac5fd31504ed0ab2fa2031f82eeea2885683f
aa.malcev2 <aa.malcev2@tensor.ru>
2019-01-15 09:14:30 +0300

    Do related to locales list tests more flexible. https://online.sbis.ru/opendoc.html?guid=084f3feb-c713-487c-8faf-0c09db618b22

--- test/Formatter/numberWords.test.js
+++ test/Formatter/numberWords.test.js
@@ -12,75 +12,104 @@ define([
 ) {
    'use strict';
 
-   describe('Types/formatter.numberWords', function() {
+   describe('Types/formatter:numberWords', function() {
+      var locales = ['en-US', 'ru-RU'];
+
       before(function () {
-         i18n.setLang('ru-RU');
-         i18n.setEnable(true);
          i18n.setDict(ruRU, 'formatter', 'ru-RU');
          i18n.setDict(enUS, 'formatter', 'en-US');
       });
-      after(function () {
-         i18n.setLang('ru-RU');
-      });
-      describe('RU', function() {
-         it('should format 0 to words', function() {
-            assert.equal('ноль', formatter.numberWords(0));
-         });
-         it('should format 2 to words', function() {
-            assert.equal('два', formatter.numberWords(2));
-         });
-         it('should format 13 to words', function() {
-            assert.equal('тринадцать', formatter.numberWords(13));
-         });
-         it('should format 23 to words', function() {
-            assert.equal('двадцать три', formatter.numberWords(23));
-         });
-         it('should format 300 to words', function() {
-            assert.equal('триста', formatter.numberWords(300));
-         });
-         it('should format 123 to words', function() {
-            assert.equal('сто двадцать три', formatter.numberWords(123));
-         });
-         it('should format 2123 to words', function() {
-            assert.equal('две тысячи сто двадцать три', formatter.numberWords(2123));
-         });
-         it('should format 23015000 to words', function() {
-            assert.equal('двадцать три миллиона пятнадцать тысяч', formatter.numberWords(23015000));
-         });
-         it('should format -6 to words', function() {
-            assert.equal('минус шесть', formatter.numberWords(-6));
-         });
-      });
-      describe('EN', function() {
-         before(function () {
-            i18n.setLang('en-US');
-         });
-         it('should format 0 to words', function() {
-            assert.equal('zero', formatter.numberWords(0));
-         });
-         it('should format 2 to words', function() {
-            assert.equal('two', formatter.numberWords(2));
-         });
-         it('should format 13 to words', function() {
-            assert.equal('thirteen', formatter.numberWords(13));
-         });
-         it('should format 23 to words', function() {
-            assert.equal('twenty-three', formatter.numberWords(23));
-         });
-         it('should format 300 to words', function() {
-            assert.equal('three hundred', formatter.numberWords(300));
-         });
-         it('should format 123 to words', function() {
-            assert.equal('one hundred and twenty-three', formatter.numberWords(123));
-         });
-         it('should format 2123 to words', function() {
-            assert.equal('two thousands, one hundred and twenty-three', formatter.numberWords(2123));
-         });
-         it('should format 23015000 to words', function() {
-            assert.equal('twenty-three millions, fifteen thousands', formatter.numberWords(23015000));
-         });
-         it('should format -6 to words', function() {
-            assert.equal('minus six', formatter.numberWords(-6));
+
+      locales.forEach(function (locale) {
+         describe('if locale "' + locale + '" is enabled', function() {
+            var stub;
+
+            beforeEach(function () {
+               var opts = new Intl.NumberFormat(locale).resolvedOptions();
+               if (opts.locale === locale) {
+                  stub = sinon.stub(i18n, 'getLang');
+                  stub.returns(locale);
+               } else {
+                  this.skip();
+               }
+            });
+
+            afterEach(function () {
+               stub.restore();
+               stub = undefined;
+            });
+
+            it('should format 0 to words', function() {
+               var expect = {
+                  'en-US': 'zero',
+                  'ru-RU': 'ноль'
+               };
+               assert.equal(expect[locale], formatter.numberWords(0));
+            });
+
+            it('should format 2 to words', function() {
+               var expect = {
+                  'en-US': 'two',
+                  'ru-RU': 'два'
+               };
+               assert.equal(expect[locale], formatter.numberWords(2));
+            });
+
+            it('should format 13 to words', function() {
+               var expect = {
+                  'en-US': 'thirteen',
+                  'ru-RU': 'тринадцать'
+               };
+               assert.equal(expect[locale], formatter.numberWords(13));
+            });
+
+            it('should format 23 to words', function() {
+               var expect = {
+                  'en-US': 'twenty-three',
+                  'ru-RU': 'двадцать три'
+               };
+               assert.equal(expect[locale], formatter.numberWords(23));
+            });
+
+            it('should format 300 to words', function() {
+               var expect = {
+                  'en-US': 'three hundred',
+                  'ru-RU': 'триста'
+               };
+               assert.equal(expect[locale], formatter.numberWords(300));
+            });
+
+            it('should format 123 to words', function() {
+               var expect = {
+                  'en-US': 'one hundred and twenty-three',
+                  'ru-RU': 'сто двадцать три'
+               };
+               assert.equal(expect[locale], formatter.numberWords(123));
+            });
+
+            it('should format 2123 to words', function() {
+               var expect = {
+                  'en-US': 'two thousand, one hundred and twenty-three',
+                  'ru-RU': 'две тысячи сто двадцать три'
+               };
+               assert.equal(expect[locale], formatter.numberWords(2123));
+            });
+
+            it('should format 23015000 to words', function() {
+               var expect = {
+                  'en-US': 'twenty-three million, fifteen thousand',
+                  'ru-RU': 'двадцать три миллиона пятнадцать тысяч'
+               };
+               assert.equal(expect[locale], formatter.numberWords(23015000));
+            });
+
+            it('should format -6 to words', function() {
+               var expect = {
+                  'en-US': 'minus six',
+                  'ru-RU': 'минус шесть'
+               };
+               assert.equal(expect[locale], formatter.numberWords(-6));
+            });
          });
       });
    });
