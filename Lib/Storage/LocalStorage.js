f7ab533982c28e61bf8ae8433b6fe6bd33381a58
aa.malcev2 <aa.malcev2@tensor.ru>
2019-02-12 08:21:24 +0300

    Add Env module.

--- Lib/Storage/LocalStorage.js
+++
@@ -1,5 +0,0 @@
-define("Lib/Storage/LocalStorage", ["require", "exports", "Browser/Storage", "Env/Env"], function (require, exports, Storage_1, Env_1) {
-    "use strict";
-    Env_1.IoC.resolve('ILogger').log("Lib/Storage/LocalStorage", 'module has been moved to "Browser/Storage:LocalStorage" and will be removed');
-    return Storage_1.LocalStorage;
-});
