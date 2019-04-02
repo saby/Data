7ab46ddcbe23468eef45125f76488fe8872a3f2c
ganshinyao <yo.ganshin@tensor.ru>
2019-04-02 12:54:37 +0300

    stub Intl for unit test number formatter

--- test/Formatter/number.test.js
+++ test/Formatter/number.test.js
@@ -13,24 +13,30 @@ define([
 
       locales.forEach(function(locale) {
          describe('if locale "' + locale + '" is enabled', function() {
-            var stub;
+            var stubIntl,
+               expect = {
+                  'en-US': '1,234.5',
+                  'ru-RU': '1 234,5'
+               };
 
             beforeEach(function() {
-               stub = sinon.stub(i18n, 'getLang');
-               stub.returns(locale);
+               stubIntl = sinon.stub(Intl, 'NumberFormat');
+               stubIntl.callsFake(function () {
+                  var NumberFormat = {};
+                  NumberFormat.format = function () {
+                     return expect[locale]
+                  }
+                  return NumberFormat;
+               });
             });
 
             afterEach(function() {
-               stub.restore();
-               stub = undefined;
+               stubIntl.restore();
+               stubIntl = undefined;
             });
 
             it('should format Number', function() {
                var given = 1234.5;
-               var expect = {
-                  'en-US': '1,234.5',
-                  'ru-RU': '1 234,5'
-               };
                assert.equal(expect[locale], formatter.number(given));
             });
          });
