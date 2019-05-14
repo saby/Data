0496bfa43fcaaaaae76200fca2452353663bb7a4
aa.malcev2 <aa.malcev2@tensor.ru>
2019-05-14 09:50:33 +0300

    Pass all static checks here.

--- Types/_formatter/number.ts
+++ Types/_formatter/number.ts
@@ -12,8 +12,8 @@ import 'i18n!Types/_formatter/number';
  *   <li>options - An object with some or all of the following properties:
  *      <ul>
  *         <li>
- *            style - The formatting style to use. Possible values are "decimal" for plain number formatting
- *            and "percent" for percent formatting; the default is "decimal".
+ *            style - The formatting style to use. Possible values are 'decimal' for plain number formatting
+ *            and 'percent' for percent formatting; the default is 'decimal'.
  *         <li>
  *            useGrouping - Whether to use grouping separators, such as thousands separators or thousand/lakh/crore
  *            separators. Possible values are true and false; the default is true.
@@ -53,7 +53,7 @@ import 'i18n!Types/_formatter/number';
  * <h2>example</h2>
  * <pre>
  *    require(['Types/_formatter/number'], function(format) {
- *       format(12325.13) // return "12,325.13" for en-US locale and "12 325.13" for ru-RU
+ *       format(12325.13) // return '12,325.13' for en-US locale and '12 325.13' for ru-RU
  *    });
  * </pre>
  * More info https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
@@ -64,126 +64,157 @@ import 'i18n!Types/_formatter/number';
 
 const NumberFormat = {
    'ru-RU': {
-      "patterns": {
-         "decimal": {
-            "positivePattern": "{number}",
-            "negativePattern": "{minusSign}{number}"
+      patterns: {
+         decimal: {
+            positivePattern: '{number}',
+            negativePattern: '{minusSign}{number}'
          },
-         "percent": {
-            "positivePattern": "{number} {percentSign}",
-            "negativePattern": "{minusSign}{number} {percentSign}"
+         percent: {
+            positivePattern: '{number} {percentSign}',
+            negativePattern: '{minusSign}{number} {percentSign}'
          }
       },
-      "symbols": {
-         "latn": {
-            "decimal": ",",
-            "group": " ",
-            "nan": "не число",
-            "plusSign": "+",
-            "minusSign": "-",
-            "percentSign": "%",
-            "infinity": "∞"
+      symbols: {
+         latn: {
+            decimal: ',',
+            group: ' ',
+            nan: 'не число',
+            plusSign: '+',
+            minusSign: '-',
+            percentSign: '%',
+            infinity: '∞'
          }
       }
    },
    'en-US': {
-      "patterns": {
-         "decimal": {
-            "positivePattern": "{number}",
-            "negativePattern": "{minusSign}{number}"
+      patterns: {
+         decimal: {
+            positivePattern: '{number}',
+            negativePattern: '{minusSign}{number}'
          },
-         "percent": {
-            "positivePattern": "{number} {percentSign}",
-            "negativePattern": "{minusSign}{number} {percentSign}"
+         percent: {
+            positivePattern: '{number} {percentSign}',
+            negativePattern: '{minusSign}{number} {percentSign}'
          }
       },
-      "symbols": {
-         "latn": {
-            "decimal": ".",
-            "group": ",",
-            "nan": "NAN",
-            "plusSign": "+",
-            "minusSign": "-",
-            "percentSign": "%",
-            "infinity": "∞"
+      symbols: {
+         latn: {
+            decimal: '.',
+            group: ',',
+            nan: 'NAN',
+            plusSign: '+',
+            minusSign: '-',
+            percentSign: '%',
+            infinity: '∞'
          }
       }
    }
 };
+
 const groupSize = 3;
 
+const DEFAULT_MAXIMUM_FRACTION_DIGITS = 21;
+type FORMAT_STYLE = 'decimal' | 'percent';
 
-function FormatNumberToString(numberFormat, x) {
-   if (numberFormat.hasOwnProperty('minimumSignificantDigits') && numberFormat.hasOwnProperty('maximumSignificantDigits')) {
-      return ToRawPrecision(x, numberFormat.minimumSignificantDigits, numberFormat.maximumSignificantDigits);
-   }
-
-   return ToRawFixed(x, numberFormat.minimumIntegerDigits || 1, numberFormat.minimumFractionDigits || 0, numberFormat.maximumFractionDigits || 21);
+interface IFormat {
+   minimumIntegerDigits?: number;
+   minimumFractionDigits?: number;
+   maximumFractionDigits?: number;
+   minimumSignificantDigits?: number;
+   maximumSignificantDigits?: number;
+   useGrouping?: boolean;
+   style?: FORMAT_STYLE;
 }
 
-function PartitionNumberPattern(numberFormat:any, x:number) {
+interface IPattern {
+   type: string;
+   value: number;
+}
 
-   let locale = i18n.getLang(),
-       localeFormat = NumberFormat[locale],
-       ild = localeFormat.symbols.latn,
-       style = numberFormat.style || 'decimal',
-       pattern;
+function formatNumberToString(numberFormat: IFormat, x: number): string {
+   if (numberFormat.hasOwnProperty('minimumSignificantDigits') &&
+      numberFormat.hasOwnProperty('maximumSignificantDigits')
+   ) {
+      return toRawPrecision(
+         x,
+         numberFormat.minimumSignificantDigits,
+         numberFormat.maximumSignificantDigits
+      );
+   }
 
+   return toRawFixed(
+      x,
+      numberFormat.minimumIntegerDigits || 1,
+      numberFormat.minimumFractionDigits || 0,
+      numberFormat.maximumFractionDigits || DEFAULT_MAXIMUM_FRACTION_DIGITS
+   );
+}
+
+function partitionNumberPattern(numberFormat: IFormat, x: number): IPattern[] {
+   const locale = i18n.getLang();
+   const localeFormat = NumberFormat[locale];
+   const ild = localeFormat.symbols.latn;
+   const style = numberFormat.style || 'decimal';
+
+   let pattern;
    if (!isNaN(x) && x < 0) {
       x = -x;
       pattern = localeFormat.patterns[style].negativePattern;
-   }
-   else {
+   } else {
       pattern = localeFormat.patterns[style].positivePattern;
    }
-   let result = [];
+
+   const result = [];
    let beginIndex = pattern.indexOf('{', 0);
    let endIndex = 0;
    let nextIndex = 0;
-   let length = pattern.length;
+   const length = pattern.length;
    while (beginIndex > -1 && beginIndex < length) {
       endIndex = pattern.indexOf('}', beginIndex);
-      if (endIndex === -1) throw new Error();
-      if (beginIndex > nextIndex) {
-         let literal = pattern.substring(nextIndex, beginIndex);
-         result.push({'type': 'literal', 'value': literal});
+      if (endIndex === -1) {
+         throw new Error();
       }
+
+      if (beginIndex > nextIndex) {
+         const literal = pattern.substring(nextIndex, beginIndex);
+         result.push({type: 'literal', value: literal});
+      }
+
       let p = pattern.substring(beginIndex + 1, endIndex);
-      if (p === "number") {
+      if (p === 'number') {
          if (isNaN(x)) {
-            let n = ild.nan;
-            result.push({'type': 'nan', 'value': n});
-         }
-         else if (!isFinite(x)) {
-            let n = ild.infinity;
-            result.push({'type': 'infinity', 'value': n});
-         }
-         else {
+            const n = ild.nan;
+            result.push({type: 'nan', value: n});
+         } else if (!isFinite(x)) {
+            const n = ild.infinity;
+            result.push({type: 'infinity', value: n});
+         } else {
             if (numberFormat.style === 'percent') {
                x *= 100;
             }
 
-            let n = FormatNumberToString(numberFormat, x);
+            const n = formatNumberToString(numberFormat, x);
 
             let integer;
             let fraction;
-            let decimalSepIndex = n.indexOf('.', 0);
+            const decimalSepIndex = n.indexOf('.', 0);
             if (decimalSepIndex > 0) {
                integer = n.substring(0, decimalSepIndex);
-               fraction = n.substring(decimalSepIndex + 1, decimalSepIndex.length);
-            }
-            else {
+               fraction = n.substring(decimalSepIndex + 1);
+            } else {
                integer = n;
                fraction = undefined;
             }
             if (numberFormat.useGrouping === true) {
-               let groupSepSymbol = ild.group;
-               let groups = [];
+               const groupSepSymbol = ild.group;
+               const groups = [];
                if (integer.length > groupSize) {
-                  let end = integer.length - groupSize;
+                  const end = integer.length - groupSize;
                   let idx = end % groupSize;
-                  let start = integer.slice(0, idx);
-                  if (start.length) groups.push(start);
+                  const start = integer.slice(0, idx);
+                  if (start.length) {
+                     groups.push(start);
+                  }
                   while (idx < end) {
                      groups.push(integer.slice(idx, idx + groupSize));
                      idx += groupSize;
@@ -196,62 +227,58 @@ function PartitionNumberPattern(numberFormat:any, x:number) {
                   throw new Error('group is empty');
                }
                while (groups.length) {
-                  let integerGroup = groups.shift();
-                  result.push({'type': 'integer', 'value': integerGroup});
+                  const integerGroup = groups.shift();
+                  result.push({type: 'integer', value: integerGroup});
                   if (groups.length) {
-                     result.push({'type': 'group', 'value': groupSepSymbol});
+                     result.push({type: 'group', value: groupSepSymbol});
                   }
                }
-            }
-            else {
-               result.push({'type': 'integer', 'value': integer});
+            } else {
+               result.push({type: 'integer', value: integer});
             }
             if (fraction !== undefined) {
-               let decimalSepSymbol = ild.decimal;
-               result.push({'type': 'decimal', 'value': decimalSepSymbol});
-               result.push({'type': 'fraction', 'value': fraction});
+               const decimalSepSymbol = ild.decimal;
+               result.push({type: 'decimal', value: decimalSepSymbol});
+               result.push({type: 'fraction', value: fraction});
             }
          }
-      }
-      else if (p === "plusSign") {
-         let plusSignSymbol = ild.plusSign;
-         result.push({'type': 'plusSign', 'value': plusSignSymbol});
-      }
-      else if (p === "minusSign") {
-         let minusSignSymbol = ild.minusSign;
-         result.push({'type': 'minusSign', 'value': minusSignSymbol});
-      }
-      else if (p === "percentSign" && numberFormat.style === "percent") {
-         let percentSignSymbol = ild.percentSign;
-         result.push({'type': 'literal', 'value': percentSignSymbol});
-      }
-      else {
-         let literal = pattern.substring(beginIndex, endIndex);
-         result.push({'type': 'literal', 'value': literal});
+      } else if (p === 'plusSign') {
+         const plusSignSymbol = ild.plusSign;
+         result.push({type: 'plusSign', value: plusSignSymbol});
+      } else if (p === 'minusSign') {
+         const minusSignSymbol = ild.minusSign;
+         result.push({type: 'minusSign', value: minusSignSymbol});
+      } else if (p === 'percentSign' && numberFormat.style === 'percent') {
+         const percentSignSymbol = ild.percentSign;
+         result.push({type: 'literal', value: percentSignSymbol});
+      } else {
+         const literal = pattern.substring(beginIndex, endIndex);
+         result.push({type: 'literal', value: literal});
       }
       nextIndex = endIndex + 1;
       beginIndex = pattern.indexOf('{', nextIndex);
    }
    if (nextIndex < length) {
-      let literal = pattern.substring(nextIndex, length);
-      result.push({'type': 'literal', 'value': literal});
+      const literal = pattern.substring(nextIndex, length);
+      result.push({type: 'literal', value: literal});
    }
+
    return result;
 }
 
 /**
- * When the ToRawPrecision abstract operation is called with arguments x (which
+ * When the toRawPrecision abstract operation is called with arguments x (which
  * must be a finite non-negative number), minPrecision, and maxPrecision (both
  * must be integers between 1 and 21) the following steps are taken:
  */
-function ToRawPrecision (x, minPrecision, maxPrecision) {
+function toRawPrecision(x: number, minPrecision: number, maxPrecision: number): string {
    let result = x.toString();
 
    if (result === '0') {
       result = Array (maxPrecision + 1).join('0');
    }
 
-   if (result.indexOf(".") >= 0 && maxPrecision > minPrecision) {
+   if (result.indexOf('.') >= 0 && maxPrecision > minPrecision) {
       let cut = maxPrecision - minPrecision;
 
       while (cut > 0 && result.charAt(result.length-1) === '0') {
@@ -259,8 +286,9 @@ function ToRawPrecision (x, minPrecision, maxPrecision) {
          cut--;
       }
 
-      if (result.charAt(result.length-1) === '.')
+      if (result.charAt(result.length - 1) === '.') {
          result = result.slice(0, -1);
+      }
    }
 
    return result;
@@ -268,75 +296,75 @@ function ToRawPrecision (x, minPrecision, maxPrecision) {
 
 /**
  * @spec[tc39/ecma402/master/spec/numberformat.html]
- * When the ToRawFixed abstract operation is called with arguments x (which must
+ * When the toRawFixed abstract operation is called with arguments x (which must
  * be a finite non-negative number), minInteger (which must be an integer between
  * 1 and 21), minFraction, and maxFraction (which must be integers between 0 and
  * 20) the following steps are taken:
  */
-function ToRawFixed(x:number, minInteger:number, minFraction:number, maxFraction:number) {
+function toRawFixed(x: number, minInteger: number, minFraction: number, maxFraction: number): string {
    // 1. Let f be maxFraction.
-   let n = Math.pow(10, maxFraction) * x; // diverging...
-   let result = (n === 0 ? "0" : n.toFixed(0)); // divering...
+   const n = Math.pow(10, maxFraction) * x; // diverging...
+   let result = (n === 0 ? '0' : n.toFixed(0)); // divering...
 
    // this diversion is needed to take into consideration big numbers, e.g.:
    // 1.2344501e+37 -> 12344501000000000000000000000000000000
    let idx;
-   let exp = (idx = result.indexOf('e')) > -1 ? +result.slice(idx + 1) : 0;
+   const exp = (idx = result.indexOf('e')) > -1 ? +result.slice(idx + 1) : 0;
    if (exp) {
       result = result.slice(0, idx).replace('.', '');
       result += Array(exp - (result.length - 1) + 1).join('0');
    }
 
    let int;
-   if (maxFraction!== 0) {
+   if (maxFraction !== 0) {
       let length = result.length;
       if (length <= maxFraction) {
-         let zero = Array(maxFraction + 1 - length + 1).join('0');
+         const zero = Array(maxFraction + 1 - length + 1).join('0');
          result = zero + result;
          length = maxFraction + 1;
       }
-      let a = result.substring(0, length - maxFraction),
-         b = result.substring(length - maxFraction, result.length);
-      result = a + "." + b;
+      const a = result.substring(0, length - maxFraction);
+      const b = result.substring(length - maxFraction, result.length);
+      result = a + '.' + b;
       int = a.length;
    } else {
       int = result.length;
    }
    let cut = maxFraction - minFraction;
-   while (cut > 0 && result.slice(-1) === "0") {
+   while (cut > 0 && result.slice(-1) === '0') {
       result = result.slice(0, -1);
       cut--;
    }
-   if (result.slice(-1) === ".") {
+   if (result.slice(-1) === '.') {
       result = result.slice(0, -1);
    }
    if (int < minInteger) {
-      let zero = Array(minInteger - int + 1).join('0');
+      const zero = Array(minInteger - int + 1).join('0');
       result = zero + result;
    }
    return result;
 }
 
-function getNumberFormat(options): any {
-   return Object.assign({
+function getNumberFormat(options: IFormat): IFormat {
+   return {...{
       style: 'decimal',
       useGrouping: true,
       minimumIntegerDigits: 0,
       minimumFractionDigits: 0,
       maximumFractionDigits: 0
-   }, options||{})
+   }, ...options || {}};
 }
+
 /*
  * @spec[tc39/ecma402/master/spec/numberformat.html]
  * @clause[sec-formatnumber]
  */
-export default function number(x: number, options?: Intl.NumberFormatOptions): string  {
-   let parts = PartitionNumberPattern(getNumberFormat(options) || {}, x);
+export default function number(x: number, options?: IFormat): string  {
+   const parts = partitionNumberPattern(getNumberFormat(options), x);
    let result = '';
    for (let i = 0; parts.length > i; i++) {
-      let part = parts[i];
+      const part = parts[i];
       result += part.value;
    }
    return result;
 }
-
