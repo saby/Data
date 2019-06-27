bcb8755d086e3b56ade92501560e8e9821a9bf0e
ganshinyao <yo.ganshin@tensor.ru>
2019-06-27 17:02:47 +0300

    unix epoch check https://online.sbis.ru/opendoc.html?guid=9e82f87d-630a-40eb-8453-04d63ba39ac8

--- Types/_formatter/dateToSql.ts
+++ Types/_formatter/dateToSql.ts
@@ -22,6 +22,8 @@ const FORMAT = {
    datetime: 'YYYY-MM-DD HH:mm:ss${ms}Z',
 };
 
+const UNIX_EPOCH_START = new Date(0);
+
 /**
  * Serializes Date to the preferred SQL format.
  * @function
@@ -33,11 +35,20 @@ const FORMAT = {
  */
 export default function toSQL(date: Date, mode: SerializationMode = MODE.DATETIME): string {
    let format = FORMAT[mode];
-   if (mode !== MODE.DATE) {
-      let ms = date.getMilliseconds() > 0 ? '.SSS' : '';
 
-      format = format.replace('${ms}', ms);
+   // There is some problem with integer timezone offsets in dates before UNIX epoch (maybe not only these ones)
+   // because we'll lose time shift and get wrong result during next fromSql() call. Let's see an example:
+   // var dt = new Date(0, 0, 1, 18, 00, 00);
+   // console.log(dt);//Mon Jan 01 1900 18:00:00 GMT+0230 (Moscow standard time)
+   // console.log(toSql(dt, 'time'));//18:00:00+02
+   // The problem is '+02' because the real shift sholud be '+02.50'
+   // We would really deal with it if we use timezone offsets with floating numbers.
+   if (mode === MODE.TIME && date < UNIX_EPOCH_START) {
+      format = format.replace('Z', '');
    }
+
+   let ms = date.getMilliseconds() > 0 ? '.SSS' : '';
+   format = format.replace('${ms}', ms);
 
    return dateFormatter(date, format);
 }
