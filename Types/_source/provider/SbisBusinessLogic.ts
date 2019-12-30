830681300fc16f8536c37954f0deb3942669bd80
aa.malcev2 <aa.malcev2@tensor.ru>
2019-12-30 15:25:32 +0300

    Limit time to produce BL calls on server to 5 seconds. https://online.sbis.ru/opendoc.html?guid=9e44cf9b-ac0c-4c12-be3e-b6141f4cfb20

--- Types/_source/provider/SbisBusinessLogic.ts
+++ Types/_source/provider/SbisBusinessLogic.ts
@@ -3,6 +3,8 @@ import {OptionsToPropertyMixin} from '../../entity';
 import {register} from '../../di';
 import {ExtendPromise} from '../../_declarations';
 import {RPCJSON} from 'Browser/Transport';
+import {constants} from 'Env/Env';
+import Deferred = require('Core/Deferred');
 
 interface IEndPoint {
     contract?: string;
@@ -11,7 +13,70 @@ interface IEndPoint {
 
 interface IOptions {
     endpoint?: IEndPoint;
-    transport?: any;
+    callTimeout?: number;
+    transport?: IRpcTransportConstructor;
+}
+
+export interface IRpcTransport {
+    callMethod<T>(method: string, args: unknown): Promise<T>;
+    abort(): void;
+}
+
+export interface IRpcTransportOptions {
+    serviceUrl: string;
+}
+
+export type IRpcTransportConstructor = new(options: IRpcTransportOptions) => IRpcTransport;
+
+// Default timeout to produce a call (in seconds)
+const DEFAULT_CALL_TIMEOUT: number = constants.isServerSide ? 5 : 0;
+
+/**
+ * Returns promise which rejecting with error if origin doesn't return any result during specified timeout.
+ * @param origin Origin promise
+ * @param timeout Timeout to wait
+ * @param methodName Called method name
+ * @param address Called address
+ */
+function getTimedOutResponse<T>(
+    origin: Promise<T> | Deferred<T>,
+    timeout: number,
+    methodName: string,
+    address: string
+): Promise<T> {
+    let result = origin;
+    const itsPromise = !(origin as Deferred<T>).isReady;
+    const timeoutMs = 1000 * timeout;
+    const timeoutError = new Error(
+        `Timeout of ${timeout} seconds has expired earlier than call for method '${methodName}' at '${address}' has returned any results.`
+    );
+
+    if (itsPromise) {
+        result = new Promise((resolve, reject) => {
+            let originHasResult = false;
+            origin.then((response) => {
+                originHasResult = true;
+                resolve(response);
+            }).catch((err) => {
+                originHasResult = true;
+                reject(err);
+            });
+
+            setTimeout(() => {
+                if (!originHasResult) {
+                    reject(timeoutError);
+                }
+            }, timeoutMs);
+        });
+    } else {
+        setTimeout(() => {
+            if ((origin as Deferred<T>).isReady()) {
+                (origin as Deferred<T>).errback(timeoutError);
+            }
+        }, timeoutMs);
+    }
+
+    return result;
 }
 
 /**
@@ -24,6 +89,8 @@ interface IOptions {
  */
 export default class SbisBusinessLogic extends OptionsToPropertyMixin implements IAbstract {
     readonly '[Types/_source/provider/IAbstract]': boolean = true;
+
+    protected _$callTimeout: number = DEFAULT_CALL_TIMEOUT;
 
     /**
      * @cfg {Endpoint} Конечная точка, обеспечивающая доступ клиента к БЛ
@@ -46,7 +113,7 @@ export default class SbisBusinessLogic extends OptionsToPropertyMixin implements
     /**
      * @cfg {Function} Конструктор сетевого транспорта
      */
-    protected _$transport: any;
+    protected _$transport: IRpcTransportConstructor = RPCJSON;
 
     /**
      * Разделитель пространств имен
@@ -68,26 +135,29 @@ export default class SbisBusinessLogic extends OptionsToPropertyMixin implements
     }
 
     call(name: string, args?: any[] | object): ExtendPromise<any> {
-        name = name + '';
-        args = args || {};
-
         const Transport = this._$transport;
         const endpoint = this.getEndpoint();
-        const overrideContract = name.indexOf('.') > -1;
 
-        if (!overrideContract && endpoint.contract) {
-            name = endpoint.contract + this._nameSpaceSeparator + name;
+        let methodName = name + '';
+        const contractIncluded = methodName.indexOf(this._nameSpaceSeparator) > -1;
+        if (!contractIncluded && endpoint.contract) {
+            methodName = endpoint.contract + this._nameSpaceSeparator + methodName;
         }
 
-        return new Transport({
+        let result = new Transport({
             serviceUrl: endpoint.address
-        }).callMethod(name, args);
+        }).callMethod(methodName, args || {}) as any;
+
+        if (result && this._$callTimeout) {
+            result = getTimedOutResponse(result, this._$callTimeout, methodName, endpoint.address);
+        }
+
+        return result;
     }
 }
 
 Object.assign(SbisBusinessLogic.prototype, {
     '[Types/_source/provider/SbisBusinessLogic]': true,
-    _$transport: RPCJSON,
     _nameSpaceSeparator: '.'
 });
 
