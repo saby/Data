286843d17eb8e56fe45f86a96c06b3afdbc83f9b
aa.malcev2 <aa.malcev2@tensor.ru>
2019-12-30 10:16:16 +0300

    Add algorithms to work with links. https://online.sbis.ru/opendoc.html?guid=8d091de6-e3be-420d-aa3b-13fe9f810e96

--- Types/_formatter/jsonReviver.ts
+++ Types/_formatter/jsonReviver.ts
@@ -1,9 +1,45 @@
+import {ILinkSignature, ISpecialSignature} from './jsonReplacer';
 import {resolve} from '../di';
+import {ISerializableSignature} from '../entity';
 
-const DataRegExp = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:[0-9\.]+Z$/;
+interface ISerializedLink {
+    name: string;
+    linkResolved?: boolean;
+    scope: object;
+    value: ILinkSignature;
+}
+
+export type ISignature = ISerializableSignature | ILinkSignature | ISpecialSignature;
+
+const DATE_MATCH = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:[0-9\.]+Z$/;
+
+let unresolvedLinks: ISerializedLink[] = [];
 let unresolvedInstances = [];
 let unresolvedInstancesId = [];
 let instanceStorage = {};
+
+function resolveLinks(): void {
+    for (let i = 0; i < unresolvedLinks.length; i++) {
+        const link = unresolvedLinks[i];
+        if (link.linkResolved) {
+            continue;
+        }
+
+        const index = unresolvedInstancesId.indexOf(link.value.id);
+        if (index === -1) {
+            throw new Error('Can\'t resolve link for property "' + link.name + '" with instance id "' + link.value.id + '".');
+        }
+        const instance = unresolvedInstances[index];
+        link.scope[link.name] = link.value = instance.value;
+        link.linkResolved = true;
+
+        // It not necessary to resolve instance if it's already resolved
+        if (!instance.instanceResolved) {
+            unresolvedInstances.splice(1 + index, 0, link);
+            unresolvedInstancesId.splice(1 + index, 0, link.value.id);
+        }
+    }
+}
 
 function resolveInstances(): void {
     let Module;
@@ -35,54 +71,63 @@ function resolveInstances(): void {
     }
 }
 
-export default function jsonReviver(name: string | number, value: any): any {
-    let result = value;
+export default function jsonReviver<T>(name: string, value: ISignature | unknown): T {
+    let result: T = value as unknown as T;
 
     if ((value instanceof Object) &&
         value.hasOwnProperty('$serialized$')
     ) {
-        switch (value.$serialized$) {
+        switch ((value as ISerializableSignature).$serialized$) {
             case 'inst':
                 unresolvedInstances.push({
                     scope: this,
                     name,
                     value
                 });
-                unresolvedInstancesId.push(value.id);
+                unresolvedInstancesId.push((value as ISerializableSignature).id);
+                break;
+            case 'link':
+                unresolvedLinks.push({
+                    scope: this,
+                    name,
+                    value: value as ILinkSignature
+                });
                 break;
             case '+inf':
-                result = Infinity;
+                result = Infinity as unknown as T;
                 break;
             case '-inf':
-                result = -Infinity;
+                result = -Infinity as unknown as T;
                 break;
             case 'undef':
                 result = undefined;
                 break;
             case 'NaN':
-                result = NaN;
+                result = NaN as unknown as T;
                 break;
             default:
-                throw new Error(`Unknown serialized type "${value.$serialized$}" detected`);
+                throw new Error(`Unknown serialized type "${(value as ISerializableSignature).$serialized$}" detected`);
         }
     }
 
-    if (typeof result === 'string') {
-        if (DataRegExp.test(result)) {
-            const dateValue = new Date(result);
-            return dateValue;
-        }
+    if (typeof result === 'string' && DATE_MATCH.test(result)) {
+        result = new Date(result) as unknown as T;
     }
 
     // Resolve links and instances at root
     if (name === '' && Object.keys(this).length === 1) {
-        resolveInstances();
-        unresolvedInstances = [];
-        unresolvedInstancesId = [];
-        instanceStorage = {};
+        try {
+            resolveLinks();
+            resolveInstances();
+        } finally {
+            unresolvedLinks = [];
+            unresolvedInstances = [];
+            unresolvedInstancesId = [];
+            instanceStorage = {};
+        }
 
         // In this case result hasn't been assigned and should be resolved from this
-        if (result === value) {
+        if (result === value as unknown as T) {
             result = this[name];
         }
     }
---
+++ TypesUnit/_formatter/jsonReviver.test.ts
@@ -0,0 +1,229 @@
+import {assert} from 'chai';
+import jsonReviver from 'Types/_formatter/jsonReviver';
+import {register, unregister} from 'Types/di';
+
+class Serializeable {
+    instanceId: number;
+    constructor() {
+        this.instanceId = Serializeable.instancesCount++;
+    }
+    toJSON(): object {
+        return {
+            $serialized$: 'inst',
+            id: this.instanceId,
+            module: 'Serializeable'
+        };
+    }
+    static instancesCount: number = 0;
+    static lastValue: unknown;
+    static fromJSON<T>(value: T): Serializeable {
+        Serializeable.lastValue = value;
+        return new Serializeable();
+    }
+}
+
+describe('Types/_formatter/jsonReviver', () => {
+    it('should deserialize a date', () => {
+       const date = new Date('1995-12-17T01:02:03');
+       const dateStr = date.toJSON();
+       const result = jsonReviver<Date>('', dateStr);
+       assert.instanceOf(result, Date);
+       assert.strictEqual(result.getTime(), date.getTime());
+    });
+
+    it('should deserialize Infinity', () => {
+       const result = jsonReviver(
+          'i',
+          jsonReviver('i', Infinity)
+       );
+       assert.strictEqual(result, Infinity);
+    });
+
+    it('should deserialize -Infinity', () => {
+       const result = jsonReviver(
+          'i',
+          jsonReviver('i', -Infinity)
+       );
+       assert.strictEqual(result, -Infinity);
+    });
+
+    it('should deserialize NaN', () => {
+       const result = jsonReviver(
+          'n',
+          jsonReviver('n', NaN)
+       );
+       assert.isNaN(result);
+    });
+
+    it('should deserialize undefined if it\'s an array element', () => {
+       const result = jsonReviver(
+          'n',
+          jsonReviver('n', undefined)
+       );
+       assert.strictEqual(result, undefined);
+    });
+
+    it('should return unchanged', () => {
+       assert.strictEqual(
+          jsonReviver(
+             'a',
+             undefined
+          ),
+          undefined
+       );
+
+       assert.strictEqual(
+          jsonReviver(
+             'a',
+             null
+          ),
+          null
+       );
+
+       assert.strictEqual(
+          jsonReviver(
+             'a',
+             1
+          ),
+          1
+       );
+
+       assert.strictEqual(
+          jsonReviver(
+             'a',
+             'b'
+          ),
+          'b'
+       );
+
+       const arr = [];
+       assert.strictEqual(
+          jsonReviver(
+             'a',
+             arr
+          ),
+          arr
+       );
+
+       const obj = {};
+       assert.strictEqual(
+          jsonReviver(
+             'a',
+             obj
+          ),
+          obj
+       );
+    });
+
+    context('when used with JSON.parse() as jsonReviver', () => {
+        it('should deserialize NaN using JSON.parse', () => {
+            const serialized = '{"$serialized$": "NaN"}';
+            const deserialized = JSON.parse(serialized, jsonReviver);
+            assert.isNaN(deserialized);
+        });
+
+        it('should work properly with deep structures', () => {
+            const given = JSON.parse(
+                `{
+                    "a":{"$serialized$":"undef"},
+                    "b":null,
+                    "c":false,
+                    "d":0,
+                    "e":1,
+                    "f":[],
+                    "g":[
+                        {"$serialized$":"undef"},
+                        1,
+                        2
+                    ],
+                    "h":{
+                        "ha":{"$serialized$":"undef"},
+                        "hb":{"$serialized$":"+inf"},
+                        "hc":{"$serialized$":"-inf"}
+                    },
+                    "j":{"$serialized$":"NaN"}
+                }`,
+                jsonReviver
+            );
+            const expected = {
+                a: undefined,
+                b: null,
+                c: false,
+                d: 0,
+                e: 1,
+                f: [],
+                g: [undefined, 1, 2],
+                h: {
+                    ha: undefined,
+                    hb: Infinity,
+                    hc: -Infinity
+                },
+                j: NaN
+            };
+
+            // 'undefined' is not serializable
+            delete expected.a;
+            delete expected.h.ha;
+
+            // Chrome doesn't create 'undefined' items even though it has a reserved indices for them
+            given.g[0] = undefined;
+
+            assert.notEqual(expected, given);
+            assert.deepEqual(expected, given);
+        });
+
+        it('should deserialize module registered with di', () => {
+            register('Serializeable', Serializeable, {instantiate: false});
+
+            const instance = new Serializeable();
+            const serialized = JSON.stringify({foo: instance});
+            const deserialized = JSON.parse(serialized, jsonReviver);
+
+            unregister('Serializeable');
+
+            assert.instanceOf(deserialized.foo, Serializeable);
+        });
+
+        it('should throw an error if module is not registered with di', () => {
+            const instance = new Serializeable();
+            const serialized = JSON.stringify({foo: instance});
+
+            assert.throws(() => {
+                JSON.parse(serialized, jsonReviver);
+            });
+        });
+
+        it('should resolve links', () => {
+            register('Serializeable', Serializeable, {instantiate: false});
+            const result = JSON.parse(
+                `{
+                    "foo": {"$serialized$": "inst", "id": 1, "module": "Serializeable"},
+                    "deep": {
+                        "bar": {"$serialized$": "link", "id": 1}
+                    }
+                }`,
+                jsonReviver
+            );
+            unregister('Serializeable');
+
+            assert.instanceOf(result.foo, Serializeable);
+            assert.strictEqual(result.foo, result.deep.bar);
+        });
+
+        it('should pass serialized value to fromJSON method', () => {
+            register('Serializeable', Serializeable, {instantiate: false});
+            JSON.parse(
+                '{"foo": {"$serialized$": "inst", "id": 0, "module": "Serializeable", "bar": "baz"}}',
+                jsonReviver
+            );
+            unregister('Serializeable');
+
+            assert.deepEqual(Serializeable.lastValue, {
+                $serialized$: 'inst',
+                id: 0,
+                module: 'Serializeable',
+                bar: 'baz'
+            });
+        });
+    });
+});
