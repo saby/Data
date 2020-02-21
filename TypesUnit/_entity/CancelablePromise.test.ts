137b23d3c02da070749efbaadf971644114dba20
aa.malcev2 <aa.malcev2@tensor.ru>
2020-02-21 16:51:57 +0300

    A brand new feature - CancelablePromise. https://online.sbis.ru/opendoc.html?guid=8aed16bc-d3ea-40a3-939b-761b9f3b7da9

---
+++ TypesUnit/_entity/CancelablePromise.test.ts
@@ -0,0 +1,67 @@
+import {assert} from 'chai';
+import CancelablePromise, {PromiseCanceledError} from 'Types/_entity/CancelablePromise';
+
+describe('Types/_entity/CancelablePromise', () => {
+    describe('promise', () => {
+        it('should return instance of Promise', () => {
+            const origin = new Promise((resolve) => resolve());
+            const instance = new CancelablePromise(origin);
+            assert.instanceOf(instance.promise, Promise);
+        });
+
+        it('should proceed resolved promise', () => {
+            const origin = new Promise((resolve) => resolve('ok'));
+            const instance = new CancelablePromise(origin);
+            return instance.promise.then((result) => {
+                assert.equal(result, 'ok');
+            });
+        });
+
+        it('should proceed rejected promise', () => {
+            const origin = new Promise((resolve, reject) => reject('fail'));
+            const instance = new CancelablePromise(origin);
+            return instance.promise.then((result) => {
+                throw new Error('Shouldn\'t get here');
+            }).catch((err) => {
+                assert.equal(err, 'fail');
+            });
+        });
+    });
+
+    describe('.cancel()', () => {
+        it('should cancel resolved promise with PromiseCanceledError', () => {
+            const origin = new Promise((resolve) => resolve());
+            const instance = new CancelablePromise(origin);
+            instance.cancel();
+            return instance.promise.then(() => {
+                throw new Error('Shouldn\'t get here');
+            }).catch((err) => {
+                assert.instanceOf(err, PromiseCanceledError);
+                assert.isTrue(err.isCanceled);
+            });
+        });
+
+        it('should cancel resolved promise with given reason', () => {
+            const origin = new Promise((resolve) => resolve());
+            const instance = new CancelablePromise(origin);
+            instance.cancel('Something wrong');
+            return instance.promise.then(() => {
+                throw new Error('Shouldn\'t get here');
+            }).catch((err) => {
+                assert.equal(err.message, 'Something wrong');
+            });
+        });
+
+        it('should cancel rejected promise with PromiseCanceledError', () => {
+            const origin = new Promise((resolve, reject) => reject());
+            const instance = new CancelablePromise(origin);
+            instance.cancel();
+            return instance.promise.then(() => {
+                throw new Error('Shouldn\'t get here');
+            }).catch((err) => {
+                assert.instanceOf(err, PromiseCanceledError);
+                assert.isTrue(err.isCanceled);
+            });
+        });
+    });
+});
