2b44c0a0732a365431cb64756382ee2edbecce5e
is.kudryavcev <is.kudryavcev@tensor.ru>
2020-02-28 11:31:24 +0300

    https://online.sbis.ru/opendoc.html?guid=a244c641-8f27-4025-bb0c-cef1f95a52ff

--- Types/_source/jsonize.ts
+++ Types/_source/jsonize.ts
@@ -29,6 +29,16 @@ function jsonizePlainObject(obj: object): object {
         }
         result[key] = newValue;
     }
+
+    if (hasChanges && obj.hasOwnProperty('toJSON') && (obj as any).toJSON !== (result as any).toJSON) {
+        Object.defineProperties(result, {
+            toJSON: {
+                enumerable: false,
+                value: (obj as any).toJSON
+            }
+        });
+    }
+
     return hasChanges ? result : obj;
 }
 
