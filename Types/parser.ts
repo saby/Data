3a181f48e862140486b9d1ca5e7d8e048b89cc19
aa.malcev2 <aa.malcev2@tensor.ru>
2020-07-08 14:16:17 +0300

    Parsers library added. https://online.sbis.ru/opendoc.html?guid=123b3bff-3c55-4d4a-a2e5-b0d8c5d89535

---
+++ Types/parser.ts
@@ -0,0 +1,17 @@
+/**
+ * Библиотека, которая создает типы из строки.
+ * @library Types/parser
+ * @includes date Types/_parser/date
+ * @public
+ * @author Мальцев А.А.
+ */
+
+/*
+ * Library that produces types from strings
+ * @library Types/parser
+ * @includes date Types/_parser/date
+ * @public
+ * @author Мальцев А.А.
+ */
+
+export {default as date} from './_parser/date';
