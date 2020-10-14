9c7acf67ad8571918ec0cda91b6863f4652e2e0e
Ярослав <t@tt.ru>
2020-10-14 19:00:48 +0300

    add comment for time interval https://online.sbis.ru/opendoc.html?guid=a7baebf7-bb36-440d-9a24-967c56459211

--- Types/_entity/compare/dateInterval.ts
+++ Types/_entity/compare/dateInterval.ts
@@ -2,6 +2,41 @@ import dateDifference, { Units } from "./dateDifference";
 
 const MONTH_IN_YEAR = 11;
 
+/**
+ * Рассчитывает количество интервалов между датами, включая граничные значения.
+ * @remark
+ * <h2>Параметры функции</h2>
+ * <ul>
+ *      <li><b>dateA</b> {Date} Первая дата.</li>
+ *      <li><b>dateB</b> {Date} Вторая дата.</li>
+ *      <li><b>[units]</b> {String} Единица расчета.</li>
+ * </ul>
+ * <h2>Возвращает</h2>
+ * {number} Количество интервалов между датами.
+ *
+ * <h2>Доступные единицы расчета.</h2>
+ * <ul>
+ *     <li>Year: год;</li>
+ *     <li>Month: месяц;</li>
+ *     <li>Day: день;</li>
+ * </ul>
+ *
+ * <h2>Примеры использования.</h2>
+ *
+ * Выведем Количество полных месяцев между датами:
+ * <pre>
+ *     import {dateInterval} from 'Types/entity';
+ *     const dateA = new Date(2019, 11, 01);
+ *     const dateB = new Date(2019, 11, 30);
+ *     console.log(compare.dateInterval(dateA, dateB, compare.DateUnits.Month)); // 1
+ * </pre>
+ *
+ * @class
+ * @name Types/_entity/compare/dateInterval
+ * @public
+ * @author Мальцев А.А.
+ */
+
 export default function dateInterval(begin: Date, end: Date, unit: Units): number {
     const diff = dateDifference(begin, end, unit);
 
